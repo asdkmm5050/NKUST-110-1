c6206691ff524c088e5c72af9f62ad0b484284b2
asdkmm5050 <asdkmm505@gmail.com>
2021-10-06 14:27:45 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import csv
+from os import device_encoding
+with open('./data/1090226.csv', newline='', encoding='utf-8') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
