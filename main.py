e700d0bc22821dfc4f5340a3c3b231527082a915
asdkmm5050 <asdkmm505@gmail.com>
2021-12-10 11:21:25 +0800

    change data and build backend main code

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-import csv
-from os import device_encoding
-with open('./data/1090226.csv', newline='', encoding='utf-8') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+import folium
+import pandas as pd
+from folium.plugins import HeatMap
+
+latitude = 22.6
+longitude = 120.3
+
+cdata = pd.read_csv('./data/NPA_TD1.csv')
+incidents = folium.map.FeatureGroup()
+latitudes = list(cdata['Latitude'])
+longitudes = list(cdata['Longitude'])
+labels = list(cdata['設置地址'])
+map = folium.Map(location=[latitude, longitude], zoom_start=10)
+
+for lat, lng, label in zip(latitudes, longitudes, labels):  # 標籤圖
+    folium.Marker([lat, lng], popup=label).add_to(map)
+
+# heatdata = cdata[['緯度','經度']].values.tolist()#熱度圖
+# HeatMap(heatdata).add_to(map)
+map.add_child(incidents)
 
