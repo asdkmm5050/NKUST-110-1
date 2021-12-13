db579ae37c888f31c16cacb68500c36031a906c3
asdkmm5050 <asdkmm505@gmail.com>
2021-12-14 02:57:14 +0800

    TL;DR add every city html, creat two generation, add a index file, lots of bug not be fixed, lazy to write readme.

---
+++ taiwan_fullmap_generation.py
@@ -0,0 +1,19 @@
+import branca
+import random
+import folium
+import json
+
+color_scale = branca.colormap.linear.YlOrRd_09.scale(0, 22)  # color scale
+taiwan_map = folium.Map(location=[25.0431, 121.539723], zoom_start=7)
+
+cities = json.load(open('./data/twCounty2010.geo.json', encoding='utf-8'))
+
+folium.Choropleth(
+    geo_data=cities,
+    style_function=lambda x: {
+        "fillColor": 'black',
+        "fillOpacity": .5,
+        "stroke": False
+    }
+).add_to(taiwan_map)
+taiwan_map.save('./html/台灣.html')
