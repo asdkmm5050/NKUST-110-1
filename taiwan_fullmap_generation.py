c4050c49f04ffe5a4937a20c00a8f9359b798bce
asdkmm5050 <asdkmm505@gmail.com>
2021-12-14 03:25:49 +0800

    modify index again

--- taiwan_fullmap_generation.py
+++ taiwan_fullmap_generation.py
@@ -4,16 +4,15 @@ import folium
 import json
 
 color_scale = branca.colormap.linear.YlOrRd_09.scale(0, 22)  # color scale
-taiwan_map = folium.Map(location=[25.0431, 121.539723], zoom_start=7)
+taiwan_map = folium.Map(location=[25.0431, 121.539723], zoom_start=7, tiles='Stamen Toner')
 
 cities = json.load(open('./data/twCounty2010.geo.json', encoding='utf-8'))
-
 folium.Choropleth(
     geo_data=cities,
     style_function=lambda x: {
-        "fillColor": 'black',
+        "fillColor": color_scale,
         "fillOpacity": .5,
-        "stroke": False
+        "stroke": True
     }
 ).add_to(taiwan_map)
 taiwan_map.save('./html/台灣.html')
